9129c6ea22b23b3fc6c49afbb5a5133a2822641a
Peed <M6xbom1@gmail.com>
2022-10-22 00:40:46 +0700

    Beta

--- food project/test.java
+++ food project/test.java
@@ -4,20 +4,22 @@ import java.util.HashMap;
 public class test{
     public static void main(String[] args) throws Exception{
 
-        int ch = 0,ch2 = 0,mch = 0;
-        String choice,choice2,mchoice,budget;
+        int ch=0,ch2=0,mch=0,rm=0;
+        String choice,choice2,mchoice,rmchoice;
+        int budget;
         HashMap<String, String> nd = new HashMap<String, String>();
 
-        budget = JOptionPane.showInputDialog("Enter your budget");
-        while(mch != 5){
+        budget = Integer.parseInt(JOptionPane.showInputDialog("Enter your budget"));
+        while(mch != 6){
             try{
                 mchoice = JOptionPane.showInputDialog(null,
                 "Choose the Option you want to perform\n"+
                 "1. Add an item\n"+
-                "2. Remove an item\n"+
+                "2. Clear Order\n"+
                 "3. Update Budget\n"+
                 "4. Show All Item\n"+
-                "5. Exit");
+                "5. Remove item\n"+
+                "6. Exit");
                 
                 mch = Integer.parseInt(mchoice);
                     switch(mch){
@@ -32,91 +34,94 @@ public class test{
                                                 "2. wide rice noodles\n" +   //เส้นใหญ่
                                                 "3. yellow Noodles\n" +      //เส้นหมี่เหลือง
                                                 "4. white rice noodles\n" +  //เส้นหมี่ขาว
-                                                "5. Back\n" +
+                                                "5. Back\n"+
                                                 "----------------------------------------------------------------\n");
                                         
                                         ch = Integer.parseInt(choice);
                                         switch (ch) {
                                             case 1:
-                                                try{
-                                                    choice2 = JOptionPane.showInputDialog(null, 
-                                                            "1.with soup\n" +
-                                                            "2.without soup\n");
-                                                    ch2 = Integer.parseInt(choice2);
-                                                    switch(ch2){
-                                                        case 1:
-                                                            nd.put("001", "narrow rice noodles with soup");
-                                                            break;
-                                                        case 2:
-                                                            nd.put("101", "narrow rice noodles without soup");
-                                                            break;
-                                                    }
-                                                }catch (Exception e){
-                                                    JOptionPane.showMessageDialog(null,e.getMessage());
-                                                }
+                                                nd.put("001", "narrow rice noodles");
+                                                // try{
+                                                //     choice2 = JOptionPane.showInputDialog(null, 
+                                                //             "1.with soup\n" +
+                                                //             "2.without soup\n");
+                                                //     ch2 = Integer.parseInt(choice2);
+                                                //     switch(ch2){
+                                                //         case 1:
+                                                //             nd.put("001", "narrow rice noodles with soup");
+                                                //             break;
+                                                //         case 2:
+                                                //             nd.put("101", "narrow rice noodles without soup");
+                                                //             break;
+                                                //     }
+                                                // }catch (Exception e){
+                                                //     JOptionPane.showMessageDialog(null,e.getMessage());
+                                                // }
                                                 break;
                         
                         
                                             case 2:
-                                                try{
-                                                    choice2 = JOptionPane.showInputDialog(null, 
-                                                            "1.with soup\n" +
-                                                            "2.without soup\n");
-                                                    ch2 = Integer.parseInt(choice2);
-                                                    switch(ch2){
-                                                        case 1:
-                                                            nd.put("002", "wide rice noodles with soup");
-                                                            break;
-                                                        case 2:
-                                                            nd.put("102", "wide rice noodles without soup");
-                                                            break;
-                                                    }
-                                                }catch (Exception e){
-                                                    JOptionPane.showMessageDialog(null,e.getMessage());
-                                                }
+                                                nd.put("002", "wide rice noodles");
+                                                // try{
+                                                //     choice2 = JOptionPane.showInputDialog(null, 
+                                                //             "1.with soup\n" +
+                                                //             "2.without soup\n");
+                                                //     ch2 = Integer.parseInt(choice2);
+                                                //     switch(ch2){
+                                                //         case 1:
+                                                //             nd.put("002", "wide rice noodles with soup");
+                                                //             break;
+                                                //         case 2:
+                                                //             nd.put("102", "wide rice noodles without soup");
+                                                //             break;
+                                                //     }
+                                                // }catch (Exception e){
+                                                //     JOptionPane.showMessageDialog(null,e.getMessage());
+                                                // }
                                                 break;
                         
                         
                                             case 3:
-                                                try{
-                                                    choice2 = JOptionPane.showInputDialog(null, 
-                                                            "1.with soup\n" +
-                                                            "2.without soup\n");
-                                                    ch2 = Integer.parseInt(choice2);
-                                                    switch(ch2){
-                                                        case 1:
-                                                            nd.put("003", "yellow Noodles with soup");
-                                                            break;
-                                                        case 2:
-                                                            nd.put("103", "yellow Noodles without soup");
-                                                            break;
-                                                    }
-                                                }catch (Exception e){
-                                                    JOptionPane.showMessageDialog(null,e.getMessage());
-                                                }
+                                                nd.put("003", "yellow Noodles");
+                                                // try{
+                                                //     choice2 = JOptionPane.showInputDialog(null, 
+                                                //             "1.with soup\n" +
+                                                //             "2.without soup\n");
+                                                //     ch2 = Integer.parseInt(choice2);
+                                                //     switch(ch2){
+                                                //         case 1:
+                                                //             nd.put("003", "yellow Noodles with soup");
+                                                //             break;
+                                                //         case 2:
+                                                //             nd.put("103", "yellow Noodles without soup");
+                                                //             break;
+                                                //     }
+                                                // }catch (Exception e){
+                                                //     JOptionPane.showMessageDialog(null,e.getMessage());
+                                                // }
                                                 break;
                         
                         
                                             case 4:
-                                                try{
-                                                    choice2 = JOptionPane.showInputDialog(null, 
-                                                            "1.with soup\n" +
-                                                            "2.without soup\n");
-                                                    ch2 = Integer.parseInt(choice2);
-                                                    switch(ch2){
-                                                        case 1:
-                                                            nd.put("004", "white rice noodles with soup");
-                                                            break;
-                                                        case 2:
-                                                            nd.put("104", "white rice noodles without soup");
-                                                            break;
-                                                    }
-                                                }catch (Exception e){
-                                                    JOptionPane.showMessageDialog(null,e.getMessage());
-                                                }
+                                                nd.put("004", "white rice noodles");
+                                                // try{
+                                                //     choice2 = JOptionPane.showInputDialog(null, 
+                                                //             "1.with soup\n" +
+                                                //             "2.without soup\n");
+                                                //     ch2 = Integer.parseInt(choice2);
+                                                //     switch(ch2){
+                                                //         case 1:
+                                                //             nd.put("004", "white rice noodles with soup");
+                                                //             break;
+                                                //         case 2:
+                                                //             nd.put("104", "white rice noodles without soup");
+                                                //             break;
+                                                //     }
+                                                // }catch (Exception e){
+                                                //     JOptionPane.showMessageDialog(null,e.getMessage());
+                                                // }
                                                 break;
-                        
-                        
+                                                
                                             case 5:
                                                 break;
                         
@@ -136,17 +141,58 @@ public class test{
                                 break;
                         
                         case 3:
-                                budget = JOptionPane.showInputDialog("Enter your budget");
+                                budget = Integer.parseInt(JOptionPane.showInputDialog("Enter your budget"));
+                                //budget
                                 JOptionPane.showMessageDialog(null,"Budget is updated");
                                 break;
 
                         case 4:
                                 for (String i : nd.values()) {
-                                    JOptionPane.showMessageDialog(null,i);
+                                    System.out.println(i);
                                 }
                                 break;
 
                         case 5:
+                                while(rm != 5){
+                                    try{
+                                        rmchoice = JOptionPane.showInputDialog(null,
+                                                "----------------------------------------------------------------\n" +
+                                                "                     Order you want to remove          \n" +
+                                                "----------------------------------------------------------------\n" +
+                                                "1. narrow rice noodles\n" + //เส้นเล็ก
+                                                "2. wide rice noodles\n" +   //เส้นใหญ่
+                                                "3. yellow Noodles\n" +      //เส้นหมี่เหลือง
+                                                "4. white rice noodles\n" +  //เส้นหมี่ขาว
+                                                "5. Back\n"+
+                                                "----------------------------------------------------------------\n");
+                                        
+                                        rm = Integer.parseInt(rmchoice);
+                                        switch (rm) {
+                                            case 1:
+                                                nd.remove("001");
+                                                break;
+                                            case 2:
+                                                nd.remove("002");
+                                                break;
+                                            case 3:
+                                                nd.remove("003");
+                                                break;
+                                            case 4:
+                                                nd.remove("004");
+                                                break;
+                                            case 5:
+                                                break;
+                                            default:
+                                                JOptionPane.showMessageDialog(null,"Invalid choice","Error",JOptionPane.ERROR_MESSAGE);
+                                                break;
+                                        }   
+                                    }catch(Exception e){
+                                        JOptionPane.showMessageDialog(null,e.getMessage());
+                                    }
+                                }
+                                break;  
+
+                        case 6:
                                 JOptionPane.showMessageDialog(null,"Program is terminated");
                                 break;
                     }
@@ -156,5 +202,15 @@ public class test{
     }
         
     }
+
+    public void getBudget(int budget){
+        if(budget <= 0){
+            JOptionPane.showMessageDialog(null,"Budget is not enough","Error",JOptionPane.ERROR_MESSAGE);
+        }else{
+            
+        }
+
+        }
 }
+
 
