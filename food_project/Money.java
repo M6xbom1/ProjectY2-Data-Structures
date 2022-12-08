3e8d96a6ff04cb497d1a29709b3657f9636222ed
Peed <M6xbom1@gmail.com>
2022-12-09 00:55:30 +0700

    fix menu

--- food_project/Money.java
+++ food_project/Money.java
@@ -12,6 +12,7 @@ public class Money implements MoneyADT {
         this(0);
     }
 
+    @Override
     public void setBath(int newBudget) throws Exception {
         if(newBudget < 0) {
             throw new Exception("Budget cannot be negative");
@@ -20,18 +21,22 @@ public class Money implements MoneyADT {
         }
     }
 
+    @Override
     public Object getBath() {
         return bath;
     }
 
+    @Override
     public boolean isEmpty() {
         return bath <= 0;
     }
 
+    @Override
     public int add(int bath) {
         return this.bath += bath;
     }
 
+    @Override
     public void remove(int bath) {
         this.bath -= bath;
     }
